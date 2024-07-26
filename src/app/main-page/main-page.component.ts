41824da52def13f107573cc4cfba67852c7b791d
phischa <ph.git@mailbox.org>
2024-07-26 22:36:34 +0200

    skills section 1

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -3,12 +3,13 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [AboutMeComponent, LandingPageComponent, PortfolioComponent, SkillsComponent, ],
+  imports: [AboutMeComponent, LandingPageComponent, PortfolioComponent, SkillsComponent, ContactComponent ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
