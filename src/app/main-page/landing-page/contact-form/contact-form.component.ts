aaf1ac7683f80671e0abd81612ad5afc1bc07e61
phischa <ph.git@mailbox.org>
2024-07-29 18:58:02 +0200

    contact form1

---
+++ src/app/main-page/landing-page/contact-form/contact-form.component.ts
@@ -0,0 +1,54 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-contact-form',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './contact-form.component.html',
+  styleUrl: './contact-form.component.scss'
+})
+export class ContactFormComponent {
+
+  http = inject(HttpClient);
+
+  contactData = {
+    name: "",
+    email: "",
+    message: "",
+  }
+
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://philip-schaper.developerakademie.net/angular-projects/contactForm/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid /* && !this.mailTest */) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+            // hier kann hinzugefügt werden, was immer an Funktionalität gewünscht ist.
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+      ngForm.resetForm();
+    }
+  }
+}
--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -3,13 +3,15 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { SkillsComponent } from './skills/skills.component';
-import { ContactComponent } from './contact/contact.component';
+import { ContactFormComponent } from './landing-page/contact-form/contact-form.component';
+
+
 
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [AboutMeComponent, LandingPageComponent, PortfolioComponent, SkillsComponent, ContactComponent ],
+  imports: [AboutMeComponent, LandingPageComponent, PortfolioComponent, SkillsComponent, ContactFormComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
