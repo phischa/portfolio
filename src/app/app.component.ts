c2801a862ed463e1faa51df7b1c3e262a7d9cb24
phischa <ph.git@mailbox.org>
2024-08-09 19:12:19 +0200

    navbar moved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 import { ImprintComponent } from './imprint/imprint.component';
 import { PrivacyComponent } from './privacy/privacy.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MainPageComponent, FooterComponent, ImprintComponent, PrivacyComponent],
+  imports: [CommonModule, RouterOutlet, MainPageComponent, FooterComponent, NavBarComponent, ImprintComponent, PrivacyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/main-page/landing-page/landing-page.component.ts
+++ src/app/main-page/landing-page/landing-page.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+/* import { NavBarComponent } from './nav-bar/nav-bar.component'; */
 
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [/* NavBarComponent */],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
